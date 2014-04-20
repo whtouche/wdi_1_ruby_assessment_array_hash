586c07399873386b5e3bba771292fb6ae1a83a93
David Fisher <tibbon@gmail.com>
2014-04-20 18:32:21 -0400

    Create array_hash.rb

---
+++ array_hash.rb
@@ -0,0 +1,20 @@
+# Question 1
+# On a new line, write code to insert the value `snake` at the end of the pets array. Don't modify the original array creation line.
+
+pets = ['cat', 'dog']
+
+# Question 2
+# Write code to return the value of the key :age in the friend hash
+friend = {name: "Chris", favorite_food: "Pizza", age: 27}
+
+# Question 3
+# Add a key of :hair_color with a value of "blue" to the friend array
+
+# Question 4
+# Write code to retrieve the index of 'dog' in the pet array.
+
+# Question 5
+# Write code to change the value of :favorite_food in the friend array to 'sushi'
+
+# Bonus
+# Explain in a comment when you would use a hash, and when you would use an array
