281807ee92a9d141b32ff416726010bcac846927
Ben White <whtouche@gmail.com>
2014-06-11 10:27:24 -0400

    Finish array / hash assessment

--- array_hash.rb
+++ array_hash.rb
@@ -1,20 +1,25 @@
-# Try to not use any reference for this one. If you need to use a reference, please note such in a comment below your answer. 
+# Try to not use any reference for this one. If you need to use a reference, please note such in a comment below your answer.
 # You'll still recieve credit, but its useful for me to know.
 
 # Question 1
 # On a new line, write code to insert the value `snake` at the end of the pets array. Don't modify the original array creation line.
 
 pets = ['cat', 'dog']
+pets << 'snake'
 
 # Question 2
 # Write code to return the value of the key :age in the friend hash
 friend = {name: "Chris", favorite_food: "Pizza", age: 27}
+puts friend[age:]
 
 # Question 3
 # Add a key of :hair_color with a value of "blue" to the friend array
+friend.push(hair_color: "blue")
 
 # Question 4
 # Write code to retrieve the index of 'dog' in the pet array.
+puts pets[1]
 
 # Question 5
 # Write code to change the value of :favorite_food in the friend array to 'sushi'
+friend[:favorite_food] = 'sushi'
