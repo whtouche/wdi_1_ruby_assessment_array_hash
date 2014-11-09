8b59eaf3a4ebc65a008963faf3b71b3d8ac58bae
Ben White <whtouche@gmail.com>
2014-11-09 14:32:38 -0500

    Cleaning up folders

--- array_hash.rb
+++ array_hash.rb
@@ -5,20 +5,26 @@
 # On a new line, write code to insert the value `snake` at the end of the pets array. Don't modify the original array creation line.
 
 pets = ['cat', 'dog']
+
 pets << 'snake'
+pets.push('snake')
+pets += ['snake']
 
 # Question 2
 # Write code to return the value of the key :age in the friend hash
 friend = {name: "Chris", favorite_food: "Pizza", age: 27}
-puts friend[age:]
+puts friend[:age]
 
 # Question 3
 # Add a key of :hair_color with a value of "blue" to the friend array
-friend.push(hair_color: "blue")
+friend[:hair_color] = 'blue'
+# XXX
 
 # Question 4
 # Write code to retrieve the index of 'dog' in the pet array.
 puts pets[1]
+pets.index('dog')
+# XXX
 
 # Question 5
 # Write code to change the value of :favorite_food in the friend array to 'sushi'
